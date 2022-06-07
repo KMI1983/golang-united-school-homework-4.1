98cfda9b659860daf6bcb86e72dcdab770e5e1c9
KMI1983 <minskkmi@gmail.com>
2022-06-08 00:56:57 +0300

    feat reverse string

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	runes := []rune(input)
+	result := []rune("")
+	for i := len(runes) - 1; i >= 0; i-- {
+		result = append(result, runes[i])
+	}
+	return string(result)
 }
